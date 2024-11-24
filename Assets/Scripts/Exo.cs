56c0315405c845de33cac2f3497a2eb63b8a4ef6
Cécile Phan Nguyen <cecilephan@outlook.com>
2024-11-24 10:47:27 +0100

    feat(assets) : adding test assets

--- Assets/Scripts/Exo.cs
+++ Assets/Scripts/Exo.cs
@@ -1,6 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Exo : MonoBehaviour
 {
 
+    private const string PLAYER_TAG = "Player";
+    [SerializeField] Animator m_Animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == PLAYER_TAG)
+        {
+            return;
+        }
+        Debug.Log("DETECT OOOOOHHH");
+        m_Animator.SetBool("isWithinPlayerRange", true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != PLAYER_TAG)
+        {
+            return;
+        }
+        Debug.Log("DETECT AAAAHHHH");
+        m_Animator.SetBool("isWithinPlayerRange", false);
+    }
 }
